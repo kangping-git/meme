d7966bb9e3a37f71cc2b6f5c90b16d2bd558a29f
Kangping <69379510+kangping-git@users.noreply.github.com>
2023-10-26 22:45:35 +0900

    Update AtCoderACIconChanger.tampermonkey.user.js

--- AtCoderACIconChanger.tampermonkey.user.js
+++ AtCoderACIconChanger.tampermonkey.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://atcoder.jp/contests/ahc025/submissions*
+// @match        https://atcoder.jp/contests/*/submissions*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atcoder.jp
 // @grant        none
 // ==/UserScript==
